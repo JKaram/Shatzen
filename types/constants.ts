0b52761b2a0e52734319322dd2f26d1824e50088
Jamie Karam <jamiekaram16@gmail.com>
2022-04-20 09:03:08 -0400

    add .5 instead of 0

--- types/constants.ts
+++ types/constants.ts
@@ -1,7 +1,6 @@
-export const possibleEstimates = [-1, 0, 1, 2, 3, 5, 8, 13];
+export const possibleEstimates = [-1, 0.5, 1, 2, 3, 5, 8, 13];
 
 export const numberToNewValue = (estimate: number) =>
   ({
-    "0": "☕",
     "-1": "?",
   }[estimate.toString()]);
