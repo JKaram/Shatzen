453ba8df69fe441a371571f17c545a93a805f58f
Jamie Karam <jamiekaram16@gmail.com>
2022-10-03 18:17:28 -0400

    add console

--- status.ts
+++ status.ts
@@ -3,5 +3,6 @@ export let appStatus = "estimating";
 export type Status = "estimating" | "revealing";
 
 export const changeStatus = (status: Status) => {
+  console.log();
   appStatus = status;
 };
