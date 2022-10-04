ccca203feac1a4728fdb7275a8af1476557c96d5
Jamie Karam <jamiekaram16@gmail.com>
2022-10-04 10:38:58 -0400

    add disabled as optional param

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
-  disabled: boolean;
+  disabled?: boolean;
   onClick: () => void;
 };
 
-export default function Button({ children, disabled, onClick }: Props) {
+export default function Button({ children, disabled = false, onClick }: Props) {
   return (
     <button
       className="py-2 bg-white border-2 border-black rounded-lg w-44 transition active:translate-y-0 hover:-translate-y-0.5 "
