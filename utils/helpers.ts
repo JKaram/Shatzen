5fc0f705984486390c268b5f9359b7e92fca2bf9
Jamie Karam <jamiekaram16@gmail.com>
2022-10-05 19:02:09 -0400

    remove helpers

--- utils/helpers.ts
+++
@@ -1,8 +0,0 @@
-import { Estimate } from "../types/aliases";
-
-export const userEstimate = (id: string | undefined, estimates: Estimate[]) => {
-  if (!id) return false;
-  const findEstimate = estimates.find((estimate) => estimate.id === id);
-  if (findEstimate) return findEstimate.estimate;
-  return false;
-};
