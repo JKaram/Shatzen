0966ce6ab90cd8d090e5113393a3d7ccdfb78e61
Jamie Karam <jamiekaram16@gmail.com>
2022-10-05 18:57:59 -0400

    remove useUser hook

--- hooks/useUser.tsx
+++
@@ -1,8 +0,0 @@
-import { Estimate } from "../types/aliases";
-
-export const useUserEstimates = (socketId: string | undefined, estimates: Estimate[]) => {
-  if (!socketId) return [false, undefined];
-  const hasEstimated = estimates.find((estimate) => estimate.id === socketId);
-  if (hasEstimated) return [!!hasEstimated, hasEstimated ? hasEstimated.estimate : undefined];
-  return [false, undefined];
-};
