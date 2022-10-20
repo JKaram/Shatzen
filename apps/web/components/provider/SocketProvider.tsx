4c2600d686e6f4fc6d8fe4e96d6bcd708e7d7632
timrobillard <hello@timrobillard.ca>
2022-10-19 23:40:32 -0400

    fix: better pinging for server health

--- apps/web/components/provider/SocketProvider.tsx
+++ apps/web/components/provider/SocketProvider.tsx
@@ -1,7 +1,8 @@
 import { Average, Status, User } from "../../types/aliases";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 import React, { createContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 type Values = {
   average: Average;
@@ -9,6 +10,12 @@ type Values = {
   serverReady: boolean;
   user: User | undefined;
   users: User[];
+  changeStatus: (status: string) => void;
+  disconnect: () => void;
+  estimate: (estimate: number) => void;
+  removeUser: () => void;
+  reset: () => void;
+  userJoin: (name: string, room: string) => void;
 };
 const initalValues: Values = {
   average: null,
@@ -16,6 +23,12 @@ const initalValues: Values = {
   serverReady: false,
   user: undefined,
   users: [],
+  changeStatus: () => undefined,
+  disconnect: () => undefined,
+  estimate: () => undefined,
+  removeUser: () => undefined,
+  reset: () => undefined,
+  userJoin: () => undefined,
 };
 
 export const SocketContext = createContext<Values>(initalValues);
@@ -24,11 +37,9 @@ type Props = {
   children: React.ReactNode;
 };
 
-const socket = io(`${process.env.NEXT_PUBLIC_SERVER}`);
-
-export default function AppProvider(props: Props) {
-  const { children } = props;
+export default function AppProvider({ children }: Props) {
   const router = useRouter();
+  const [socket, setSocket] = useState<Socket>();
   const [average, setAverage] = useState<Average>(null);
   // Server goes to sleep and may take a few seconds to wake up.
   const [serverReady, setServerReady] = useState(false);
@@ -37,23 +48,80 @@ export default function AppProvider(props: Props) {
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    socket.on("connect", () => setServerReady(true));
+    if (!serverReady) return;
+    const newSocket = io(`${process.env.NEXT_PUBLIC_SERVER}`);
+    newSocket.on("connect", () => console.log("hello"));
+    newSocket.on("connect_error", (err) => console.log("err", err));
 
-    socket.on("users", (users: User[]) => {
-      const sortedUsers = users
-        .filter((estimate) => estimate.id !== socket.id)
+    newSocket.on("users", (users: User[]) => {
+      const sortedUsers = Object.values(users)
+        .filter((estimate) => estimate.id !== newSocket.id)
         .sort((a, b) => (a.id === b.id ? 1 : 0));
-      const currentUser = users.find((user) => user.id === socket.id);
+      const currentUser = users[newSocket.id];
       if (currentUser) sortedUsers.unshift(currentUser);
       setUser(currentUser);
       setUsers(sortedUsers);
     });
 
-    socket.on("firstConnect", (roomId) => router.push(`/room/${roomId}`));
+    newSocket.on("firstConnect", (roomId) => router.push(`/room/${roomId}`));
 
-    socket.on("average", (average: Average) => setAverage(average));
-    socket.on("roomStatus", (status: Status) => setStatus(status));
-  }, [socket]);
+    newSocket.on("average", (average: Average) => setAverage(average));
+    newSocket.on("roomStatus", (status: Status) => setStatus(status));
+    setSocket(newSocket);
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [serverReady]);
+
+  const changeStatus = (status: string) => {
+    socket.emit("changeStatus", { status });
+  };
+  const disconnect = () => {
+    socket.disconnect();
+  };
+  const estimate = (estimate: number) => {
+    socket.emit("estimate", { estimate });
+  };
+  const removeUser = () => {
+    socket.emit("removeUser", {});
+  };
+  const reset = () => {
+    socket.emit("reset");
+  };
+  const userJoin = (name: string, room: string) => {
+    socket.emit("userJoin", { name, room });
+  };
+
+  const checkHealth = async () => {
+    const healthUrl = `${process.env.NEXT_PUBLIC_SERVER}/health`;
+    try {
+      await axios.get(healthUrl);
+      setServerReady(true);
+      return null;
+    } catch (error) {
+      let tries = 5;
+      const interval = setInterval(async () => {
+        try {
+          await axios.get(healthUrl);
+          setServerReady(true);
+          clearInterval(interval);
+        } catch (error) {
+          console.log("error", error);
+        }
+        tries -= 1;
+        if (tries < 1) clearInterval(interval);
+      }, 2000);
+      return interval;
+    }
+  };
+
+  useEffect(() => {
+    const interval = checkHealth();
+
+    return () => {
+      interval.then((inv) => clearInterval(inv));
+    };
+  }, []);
 
   return (
     <SocketContext.Provider
@@ -63,6 +131,12 @@ export default function AppProvider(props: Props) {
         serverReady: serverReady,
         user: user,
         users: users,
+        changeStatus,
+        disconnect,
+        estimate,
+        removeUser,
+        reset,
+        userJoin,
       }}
     >
       {children}
@@ -70,17 +144,4 @@ export default function AppProvider(props: Props) {
   );
 }
 
-export const useSockets = () => {
-  const changeStatus = (status: string) =>
-    socket.emit("changeStatus", { status });
-  const disconnect = () => socket.disconnect();
-  const estimate = (estimate: number) => socket.emit("estimate", { estimate });
-  const removeUser = () => {
-    socket.emit("removeUser", {});
-  };
-  const reset = () => socket.emit("reset");
-  const userJoin = (name: string, room: string) =>
-    socket.emit("userJoin", { name, room });
-
-  return { changeStatus, disconnect, estimate, removeUser, reset, userJoin };
-};
+export const useSockets = () => React.useContext(SocketContext);
