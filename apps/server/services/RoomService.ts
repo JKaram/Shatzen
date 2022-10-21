b48cdbad80fc5ef0f2d9a2eab25e1bb3bdb61536
timrobillard <hello@timrobillard.ca>
2022-10-20 23:45:17 -0400

    chore: better types BE

--- apps/server/services/RoomService.ts
+++ apps/server/services/RoomService.ts
@@ -1,39 +1,44 @@
 import { Server, Socket } from "socket.io";
-import { Estimate, Status, User } from "../types";
-import { chooseUserColour } from "../utils";
+import {
+  EmitFunction,
+  SocketIncomingEvents,
+  SocketOutgoingEvents,
+} from "../types";
+import { Room, User } from "../types";
+import { chooseUserColour } from "../utils/chooseUserColour";
 
-class Room {
-  private store: any;
+class RoomService {
   private maxUsers = 0;
+  private store: Set<string> & { users: Record<string, User>; room: Room };
 
   constructor(
-    private io: Server,
-    private socket: Socket,
+    private io: Server<SocketIncomingEvents, SocketOutgoingEvents>,
+    private socket: Socket<SocketIncomingEvents, SocketOutgoingEvents>,
     private roomId: string,
     private username: string
-  ) {
-    this.store = io.of("/").adapter;
-  }
+  ) {}
 
   async init() {
     const clients = await this.io.in(this.roomId).fetchSockets();
     if (!clients) console.error(`INTERNAL ERROR - Room creation failed`);
 
     await this.socket.join(this.roomId);
-    this.store = this.store.rooms.get(this.roomId);
+    // Next line returns a set, which we mutate with users and room data
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    this.store = this.io.of("/").adapter.rooms.get(this.roomId) as any;
 
     const user: User = {
       colour: chooseUserColour(),
       estimate: null,
       id: this.socket.id,
       name: this.username,
-      roomId: this.roomId,
     };
     if (clients.length === 0) {
       this.store.users = {
         [this.socket.id]: user,
       };
       this.store.room = {
+        id: this.roomId,
         average: -1,
         status: "estimating",
       };
@@ -48,22 +53,22 @@ class Room {
   // Emitters
 
   emitUsers() {
-    this.emitToRoom("users", this.store.users);
+    this.emitToRoom("users", [this.store.users]);
   }
   emitStatus(toSelf?: boolean) {
-    this.emit("roomStatus", this.store.room.status, toSelf);
+    this.emit("roomStatus", [this.store.room.status], toSelf);
   }
   emitAverage(toSelf?: boolean) {
-    this.emit("average", this.store.room.average, toSelf);
+    this.emit("average", [this.store.room.average], toSelf);
   }
   emitFirstConnect() {
-    this.emitToSelf("firstConnect", this.roomId);
+    this.emitToSelf("firstConnect", [this.roomId]);
   }
 
   // Listeners
 
   onChangeStatus() {
-    this.socket.on("changeStatus", ({ status }: { status: Status }) => {
+    this.socket.on("changeStatus", ({ status }) => {
       this.store.room.status = status;
 
       let totalCount = 0;
@@ -94,7 +99,7 @@ class Room {
   }
 
   onEstimate() {
-    this.socket.on("estimate", ({ estimate }: { estimate: Estimate }) => {
+    this.socket.on("estimate", ({ estimate }) => {
       this.store.users[this.socket.id].estimate = estimate;
       this.emitUsers();
     });
@@ -107,17 +112,21 @@ class Room {
     });
   }
 
-  private emit(message: string, data: any, toSelf?: boolean) {
-    this[toSelf ? "emitToSelf" : "emitToRoom"](message, data);
-  }
+  private emit: EmitFunction = (message, data, toSelf) => {
+    if (toSelf) {
+      this.emitToSelf(message, data);
+      return;
+    }
+    this.emitToRoom(message, data);
+  };
 
-  private emitToSelf(message: string, data: any) {
-    this.io.to(this.socket.id).emit(message, data);
-  }
+  private emitToSelf: EmitFunction = (message, data) => {
+    this.io.to(this.socket.id).emit(message, ...data);
+  };
 
-  private emitToRoom(message: string, data: any) {
-    this.io.to(this.roomId).emit(message, data);
-  }
+  private emitToRoom: EmitFunction = (message, data) => {
+    this.io.to(this.roomId).emit(message, ...data);
+  };
 
   onDisconnect() {
     this.socket.on("disconnect", () => {
@@ -132,8 +141,10 @@ class Room {
   }
 
   private addToMax() {
-    if (this.store.users.length === this.maxUsers) this.maxUsers += 1;
+    // TODO use for metrics
+    if (Object.values(this.store.users).length === this.maxUsers)
+      this.maxUsers += 1;
   }
 }
 
-export default Room;
+export default RoomService;
