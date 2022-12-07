5be472fba0991fc582742b2107559f963e1faabc
Ted Morin <morinted@gmail.com>
2022-12-07 14:02:05 -0500

    feat(web): Add random card back colour and style

--- apps/web/hooks/useCardBuilder.tsx
+++ apps/web/hooks/useCardBuilder.tsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
 import { useSockets } from "../providers/SocketProvider";
-import { UserStorage } from "../types/aliases";
-import { USER_COLOURS } from "../types/constants";
-
+import { UserCustoms } from "../types/aliases";
+import { USER_CARD_PATTERNS, USER_COLOURS } from "../types/constants";
 import useLocalStorage from "./useLocalStorage";
 
 export const useCardBuilder = () => {
   const { user } = useSockets();
-  const [previous] = useLocalStorage<UserStorage | undefined>(
+  const [previous] = useLocalStorage<UserCustoms | undefined>(
     "user",
     undefined
   );
   const [selected, setSelected] = useState({
-    colour: previous?.colour || user?.colour || USER_COLOURS[0],
-    pattern: previous?.pattern || user?.pattern || -1,
-    name: "",
+    colour: previous?.colour || user?.colour || USER_COLOURS[Math.floor(Math.random() * USER_COLOURS.length)],
+    pattern: previous?.pattern || user?.pattern || Math.floor(Math.random() * (USER_CARD_PATTERNS.length + 1)) - 1,
   });
 
   return { selected, setSelected };
