31df038fa9ed8a10560362cdc63057089131c610
Ted Morin <morinted@gmail.com>
2022-12-07 13:06:34 -0500

    feat: Add card flip on reveal

--- apps/web/components/UserCard.tsx
+++ apps/web/components/UserCard.tsx
@@ -16,35 +16,59 @@ export const UserCard = ({ user }: Props) => {
 
   return (
     <div className="flex flex-col items-center">
-      <div
-        className={classNames(
-          "w-10 h-16 bg-white rounded flex justify-center items-center",
-          estimate ? "border-2 border-black" : "shadow-inset bg-[#d0d0d0]"
-        )}
-        style={{
-          backgroundImage:
-            user.pattern >= 0 && userHasEstimated && roomStatus === 'estimating'
-              ? `url("${USER_CARD_PATTERNS[user.pattern]}")`
-              : undefined,
-          backgroundColor: userHasEstimated && roomStatus === 'estimating' ? user?.colour : undefined,
-          borderColor: roomStatus === 'revealing' ? user?.colour : undefined
-        }}
-      >
-        <span
-          className={classNames(
-            "text-center font-bold text-xl",
-            roomStatus === "estimating"
-              ? "hidden"
-              : "block"
-          )}
+      {userHasEstimated ? (
+        <div
+          className={classNames("card", "w-10 h-16 bg-transparent")}
+          style={{ perspective: 1000 }}
         >
-          {estimate ? numberToNewValue(estimate) : null}
-        </span>
-      </div>
-      <span
-        className={classNames("px-2 mt-2")}
-        title={`User ID: ${user.id}`}
-      >
+          <div
+            className={classNames(
+              "card-inner",
+              "relative w-full h-full",
+              "transition transform duration-500",
+            )}
+            style={{ transformStyle: "preserve-3d", transform: roomStatus === 'revealing' ? 'rotateY(180deg)' : undefined }}
+          >
+            <div
+              className={classNames(
+                "card-back",
+                "absolute w-full h-full bg-white rounded flex justify-center items-center",
+                estimate ? "border-2 border-black" : "shadow-inset bg-[#d0d0d0]"
+              )}
+              style={{
+                backgroundImage:
+                  user.pattern >= 0 &&
+                  `url("${USER_CARD_PATTERNS[user.pattern]}")`,
+                backgroundColor: user?.colour,
+                backfaceVisibility: "hidden"
+              }}
+            />
+            <div
+              className={classNames(
+                "card-front",
+                "absolute w-full h-full bg-white rounded flex justify-center items-center border-2 border-black"
+              )}
+              style={{
+                borderColor: user?.colour,
+                backfaceVisibility: "hidden",
+                transform: 'rotateY(180deg)'
+              }}
+            >
+              <span className={classNames("text-center font-bold text-xl")}>
+                {estimate ? numberToNewValue(estimate) : null}
+              </span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={classNames(
+            "no-card",
+            "w-10 h-16 bg-[#d0d0d0] shadow-inset rounded flex justify-center items-center"
+          )}
+        />
+      )}
+      <span className={classNames("px-2 mt-2")} title={`User ID: ${user.id}`}>
         {user.name}
       </span>
     </div>
