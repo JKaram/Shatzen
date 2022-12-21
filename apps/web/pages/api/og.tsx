ed89e1acfed1b18cdc995d6d9e76202c34094dcc
JKaram <jamiekaram16@gmail.com>
2022-12-21 15:15:17 -0500

    fix: type check string

--- apps/web/pages/api/og.tsx
+++ apps/web/pages/api/og.tsx
@@ -5,7 +5,7 @@ export const config = {
 };
 
 const font = fetch(
-  new URL("../../public/fonts/Roboto-Bold.ttf", import.meta.url)
+  new URL("../../public/fonts/Roboto-Bold.ttf" as string, import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 export default async function handler() {
