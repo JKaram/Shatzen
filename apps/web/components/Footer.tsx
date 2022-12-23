3b5db57a8812f8ea6340f9e94be6d2c98b832603
Jamie Karam <jamiekaram16@gmail.com>
2022-12-23 15:13:29 -0500

    fix: add a fixed position to footer (#21)

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bottom-0 left-0 flex items-end justify-end w-full h-12 p-1 text-gray-500">
+    <footer className="fixed bottom-0 left-0 flex items-end justify-end w-full h-12 p-1 text-gray-500">
       <span>
         Contact me at&nbsp;
         <a
